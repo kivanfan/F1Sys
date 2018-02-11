a21ffee118da4dc54becae6d1615813e33826f47
kivanfan <kivan011040@gmail.com>
2018-02-12 03:07:15 +0800

    init

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fan
+ * Date: 11/2/2018
+ * Time: 23:52
+ */
+?>
+<link rel="stylesheet" href="/css/app.css">
+<script src="/js/app.js"></script>
+<div class="row">
+    <div class="col-6">
+        <i class="fas fa-user"></i>
+    </div>
+    <div class="col-6">
+        test2
+    </div>
+</div>
